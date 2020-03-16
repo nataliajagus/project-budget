7c29cd42faeec6565fd34cc834b706cf23ff8340
Natalia Jaguś <natalia.jagus@gmail.com>
2020-03-16 22:44:16 +0100

    update Project component

---
+++ app/src/views/Project.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import GlobalStyle from 'theme/GlobalStyle';
+import ProjectHeader from 'components/molecules/ProjectHeader/ProjectHeader';
+
+
+const Project = () => (
+    <>  
+        <GlobalStyle />
+        <ProjectHeader />
+    </>
+)
+
+export default Project;
