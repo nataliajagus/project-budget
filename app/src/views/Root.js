094af10323b0d2488e1b80b4dd463be7ccd60625
Natalia Jaguś <natalia.jagus@gmail.com>
2020-03-16 22:44:24 +0100

    add react router

--- app/src/views/Root.js
+++ app/src/views/Root.js
@@ -7,53 +7,25 @@ import SearchBar from 'components/atoms/SearchBar/SearchBar';
 import GreenButton from 'components/atoms/GreenButton/GreenButton';
 import Plus from 'assets/plus.svg';
 
-const ProjectsWrapper = styled.div`
-    
-    display: flex;
-    margin: 0 auto;
-    @media (min-width: 992px) {
-        justify-content: space-between;
-    }
+import Project from 'views/Project.js'
+import AllProjects from 'views/AllProjects.js'
 
-    margin-top: 50px;
-    flex-wrap: wrap;
-    justify-content: center;
-    max-width: 950px;
-`;
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  
 
-const RowWrapper = styled.div`
-    max-width: 300px;
-    display: flex;
-    align-items: center;
-    margin: 50px auto;
-    justify-content: space-between;
-
-    @media (min-width: 666px) {
-        max-width: 620px;
-    }
-
-    @media (min-width: 992px) {
-        max-width: 950px;
-    }
-`;
 
 const Root = () => (
-    <>  
-        <RowWrapper>
-            <SearchBar placeholder="Szukaj projektu..." />
-            <GreenButton icon={Plus} />
-        </RowWrapper>
-        
-        <ProjectsWrapper>
-            <GlobalStyle />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-        </ProjectsWrapper>
-    </>
+    <Router>  
+        <Switch>
+            <Route exact path="/" component={AllProjects} />
+            <Route path="/project" component={Project} />
+        </Switch>
+    </Router>
 )
 
 export default Root;
