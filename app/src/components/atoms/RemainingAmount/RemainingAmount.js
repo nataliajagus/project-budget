493adc951d41b0f372e6bedc0b2c5df0b00eb423
Natalia Jaguś <natalia.jagus@gmail.com>
2020-03-16 22:43:23 +0100

    add RemainingAmount component

---
+++ app/src/components/atoms/RemainingAmount/RemainingAmount.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const RemainingAmount = styled.span`
+    font-size: 3rem;
+    color: #292929;
+    font-weight: bold;
+    text-align: center;
+    margin-right: 15px;
+`;
+
+export default RemainingAmount;
